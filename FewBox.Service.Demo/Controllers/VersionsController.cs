a83b4afc7cd39ca4756d7f2cf4a4a5f26898b43e
Peter Pang <mv@live.cn>
2020-06-28 13:10:58 +0800

    Version 1.

--- FewBox.Service.Demo/Controllers/VersionsController.cs
+++ FewBox.Service.Demo/Controllers/VersionsController.cs
@@ -16,7 +16,7 @@ namespace FewBox.Service.Demo.Controllers
         {
             return new PayloadResponseDto<string>
             {
-                Payload ="latest"
+                Payload ="v1"
             };
         }
     }
