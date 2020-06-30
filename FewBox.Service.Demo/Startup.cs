15edcfede364b605e2afe36afc69ff7740380e86
Peter Pang <mv@live.cn>
2020-06-30 09:51:15 +0800

    Resolve multi ReDoc issue.

--- FewBox.Service.Demo/Startup.cs
+++ FewBox.Service.Demo/Startup.cs
@@ -191,7 +191,8 @@ namespace FewBox.Service.Demo
             }
             if (env.IsProduction())
             {
-                app.UseReDoc();
+                app.UseReDoc(c => c.DocumentPath = "/swagger/v1/swagger.json");
+                app.UseReDoc(c => c.DocumentPath = "/swagger/v2/swagger.json");
                 app.UseHsts();
             }
             app.UseEndpoints(endpoints =>
